371d563cbec7584a8bbedda5b207e0c23592283f
Mikit Masliyevich <mikitmasliyevich@Mikits-MacBook-Pro.local>
2021-11-16 10:55:51 +0300

    feat: add jsx

--- src/pages/docs/json-schemas.tsx
+++ src/pages/docs/json-schemas.tsx
@@ -1,8 +1,28 @@
 import { PageComponentWithLayout } from "../../types";
 import DocsLayout from "components/Documentation/Layout/DocsLayout";
 
+type JSONSchemas = {
+    // props
+};
 const JSONSchemas: PageComponentWithLayout = () => {
-    return <div>JSON Schemas</div>;
+    return(
+    <> 
+    <div className="page-content">
+    <h1 className="doc-main-title">JSON Schemas</h1>
+     <p>
+     Our API is defined by 
+        <a  target="_blank" href="https://github.com/binary-com/websockets/tree/gh-pages/config"> JSON Schemas</a>. 
+        Get updates by looking for "JSON Schema Update" in the 
+        <a target="_blank"  href="https://github.com/binary-com/websockets/commits/gh-pages"> changelog</a>.
+    </p>
+    <h3>Useful tools:</h3>
+    <p><a target="_blank" href="http://jeremydorn.com/json-editor/" >JSON Editor with JSON Schema support</a></p>
+    <p><a target="_blank" href="https://lbovet.github.io/typson-demo/">Generate JSON Schema from TypeScript</a></p>
+    <p><a target="_blank" href="http://jsonschema.net/">Generate JSON Schema from JSON Object</a></p>
+    <p><a target="_blank" href="http://www.jsonschemavalidator.net/">JSON Schema Validator</a></p>
+    </div>
+    </>
+    );
 };
 
 JSONSchemas.Layout = DocsLayout;

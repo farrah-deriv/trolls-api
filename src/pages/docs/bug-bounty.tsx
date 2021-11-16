84989104e734931abeb6549258e9404e8aaebe25
Mikit Masliyevich <mikitmasliyevich@Mikits-MacBook-Pro.local>
2021-11-16 11:15:30 +0300

    feat: fixed EsLint errors

--- src/pages/docs/bug-bounty.tsx
+++ src/pages/docs/bug-bounty.tsx
@@ -7,15 +7,15 @@ const BugBounty = () => {
         <Section>
             <div className="page-content">
                 <h1 className="doc-main-title">Bug bounty</h1>
-    <div className="text-block">
-    <h3 className="mb-8p bb-m-sub-title">Want to help us enhance our security?</h3>
-    <p>
+                <div className="text-block">
+                    <h3 className="mb-8p bb-m-sub-title">Want to help us enhance our security?</h3>
+                    <p>
       Test our products and services for security vulnerabilities and earn a
       monetary reward for any verifiable issues that you find, courtesy of our
       bug bounty programme.
-    </p>
-    </div>
-            <FeedbackSection id="bugBounty" />
+                    </p>
+                </div>
+                <FeedbackSection id="bugBounty" />
             </div>
         </Section>
     );
--- src/pages/docs/json-schemas.tsx
+++ src/pages/docs/json-schemas.tsx
@@ -6,22 +6,22 @@ type JSONSchemas = {
 };
 const JSONSchemas: PageComponentWithLayout = () => {
     return(
-    <> 
-    <div className="page-content">
-    <h1 className="doc-main-title">JSON Schemas</h1>
-     <p>
+        <> 
+            <div className="page-content">
+                <h1 className="doc-main-title">JSON Schemas</h1>
+                <p>
      Our API is defined by 
-        <a  target="_blank" href="https://github.com/binary-com/websockets/tree/gh-pages/config"> JSON Schemas</a>. 
-        Get updates by looking for "JSON Schema Update" in the 
-        <a target="_blank"  href="https://github.com/binary-com/websockets/commits/gh-pages"> changelog</a>.
-    </p>
-    <h3>Useful tools:</h3>
-    <p><a target="_blank" href="http://jeremydorn.com/json-editor/" >JSON Editor with JSON Schema support</a></p>
-    <p><a target="_blank" href="https://lbovet.github.io/typson-demo/">Generate JSON Schema from TypeScript</a></p>
-    <p><a target="_blank" href="http://jsonschema.net/">Generate JSON Schema from JSON Object</a></p>
-    <p><a target="_blank" href="http://www.jsonschemavalidator.net/">JSON Schema Validator</a></p>
-    </div>
-    </>
+                    <a  target="_blank" href="https://github.com/binary-com/websockets/tree/gh-pages/config" rel="noreferrer"> JSON Schemas</a>. 
+        Get updates by looking for JSON Schema Update in the 
+                    <a target="_blank"  href="https://github.com/binary-com/websockets/commits/gh-pages" rel="noreferrer"> changelog</a>.
+                </p>
+                <h3>Useful tools:</h3>
+                <p><a target="_blank" href="http://jeremydorn.com/json-editor/" rel="noreferrer" >JSON Editor with JSON Schema support</a></p>
+                <p><a target="_blank" href="https://lbovet.github.io/typson-demo/" rel="noreferrer">Generate JSON Schema from TypeScript</a></p>
+                <p><a target="_blank" href="http://jsonschema.net/" rel="noreferrer">Generate JSON Schema from JSON Object</a></p>
+                <p><a target="_blank" href="http://www.jsonschemavalidator.net/" rel="noreferrer">JSON Schema Validator</a></p>
+            </div>
+        </>
     );
 };
 

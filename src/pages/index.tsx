4888ee76b1f1b4bf54c180cdfffe0897a7931559
Gena888 <theGena888@gmail.com>
2021-11-16 17:48:01 +0300

    feat: add ways and getStarted to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
-import Section from "components/Home/Section/Section";
-
+import WaysApi from "components/Home/Sections/DerivApi/WaysApi";
+import GetStarted from "components/Home/Sections/GetStarted/GetStarted"
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      {/* <Section/>
-    <Section/>
-    <Section/>
-    <Section/> */}
-    </div>
-  );
+    return (
+        <div>
+            <WaysApi />
+            <GetStarted />
+        </div>
+    );
 };
 
 export default Home;

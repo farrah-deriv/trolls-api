f7ec3a4a609c3a7ead569d3548315f98289f22c2
Mikit Masliyevich <mikitmasliyevich@Mikits-MacBook-Pro.local>
2021-11-17 11:07:40 +0300

    feat: add Link for all navigation

--- src/components/Documentation/Layout/JsonSchemas/JsonSchemasComponent.tsx
+++ src/components/Documentation/Layout/JsonSchemas/JsonSchemasComponent.tsx
@@ -1,27 +1,39 @@
 import React from "react"
-import s from "./JsonSchemasComponent.module.scss"
+import style from "./JsonSchemasComponent.module.scss"
+import Link from "next/link";
 
-type JsonSchemasComponent = {
-   //props
-};
 
 const JsonSchemasComponent: React.FC = () => {
-    const title = " JSON Schemas";
     
+    const json_navigation = [
+        { id: "json-schemas", title: "JSON Schemas", path: "https://github.com/binary-com/websockets/tree/gh-pages/config" },
+        { id: "changelog", title: "changelog", path: "https://github.com/binary-com/websockets/commits/gh-pages" },
+        { id: "json-editor", title: "JSON Editor with JSON Schema support", path: "https://jeremydorn.com/json-editor/" },
+        { id: "json-schema_ts", title: "Generate JSON Schema from TypeScript", path: "https://lbovet.github.io/typson-demo/" },
+        { id: "json-schema_object", title: "Generate JSON Schema from JSON Object", path: "https://jsonschema.net/" },
+        { id: "json-schema_validator", title: "JSON Schema Validator", path: "https://www.jsonschemavalidator.net/" },
+    ];
+
     return(
-        <div className={s["page-container"]}>
-            <h1 className={s["doc-main-title"]}>{title}</h1>
+        <div className={style["page-container"]}>
+            <h1 className={style["doc-main-title"]}>JSON Schemas</h1>
             <p>
      Our API is defined by 
-                <a  target="_blank" href="https://github.com/binary-com/websockets/tree/gh-pages/config" rel="noreferrer">{title}</a>. 
+                <Link href={json_navigation[0].path}><a target="_blank" rel="noreferrer"> {json_navigation[0].title}</a></Link>. 
         Get updates by looking for JSON Schema Update in the 
-                <a target="_blank"  href="https://github.com/binary-com/websockets/commits/gh-pages" rel="noreferrer"> changelog</a>.
+                <Link href={json_navigation[1].path}><a target="_blank" rel="noreferrer"> {json_navigation[1].title}</a></Link>. 
             </p>
             <h3>Useful tools:</h3>
-            <p><a target="_blank" href="https://jeremydorn.com/json-editor/" rel="noreferrer" >JSON Editor with JSON Schema support</a></p>
-            <p><a target="_blank" href="https://lbovet.github.io/typson-demo/" rel="noreferrer">Generate JSON Schema from TypeScript</a></p>
-            <p><a target="_blank" href="https://jsonschema.net/" rel="noreferrer">Generate JSON Schema from JSON Object</a></p>
-            <p><a target="_blank" href="https://www.jsonschemavalidator.net/" rel="noreferrer">JSON Schema Validator</a></p>
+            {json_navigation.map(
+                ({ id, title, path }, i) =>
+                    i > 1 && (
+                        <p>
+                            <Link key={id} href={path}>
+                                <a target="_blank" rel="noreferrer">{title}</a>
+                            </Link>
+                        </p>
+                    )
+            )}
         </div>
     );
 };

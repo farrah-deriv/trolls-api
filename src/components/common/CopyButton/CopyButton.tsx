ffdac0f89950a6197cd8f9e6e8632d717b406e72
amina-deriv <amina@deriv.com>
2021-11-18 16:35:49 +0400

    copybutton for api guide page

--- src/components/common/CopyButton/CopyButton.tsx
+++ src/components/common/CopyButton/CopyButton.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 import styles from "./CopyButton.module.scss";
 
 type CopyButtonProps = {
-    size: string,
+    size?: string,
     content_to_copy: string
+    className?: string
 }
 
-const CopyButton: React.FC<CopyButtonProps> = ({ size = "16", content_to_copy }) => {
-
+const CopyButton: React.FC<CopyButtonProps> = ({ size = "16", content_to_copy, className = "copy_button" }) => {
     const fallbackCopyTextToClipboard = () => {
         const dummy = document.createElement("textarea");
         document.body.appendChild(dummy);
@@ -25,10 +25,10 @@ const CopyButton: React.FC<CopyButtonProps> = ({ size = "16", content_to_copy })
         navigator.clipboard.writeText(content_to_copy);
     }
     return (
-        <div className={styles.copy_button} onClick={handleCopyButtonClick}>
+        <div className={styles[`${className}`]} onClick={handleCopyButtonClick} >
             <Image className={styles.copy_button_image} src="/copy.svg" width={size} height={size} alt="copy code icon" />
-            <span className={styles.copy_button_text}> Copy </span>
-        </div>
+            <div className={styles.copy_button_text}> Copy </div>
+        </div >
     )
 };
 

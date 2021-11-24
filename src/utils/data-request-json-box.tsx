311396b92c86bd4ce183d707453cbd67b8003746
amina-deriv <amina@deriv.com>
2021-11-24 12:54:32 +0400

    remove code smells

--- src/utils/data-request-json-box.tsx
+++ src/utils/data-request-json-box.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Button from "components/common/Button/Button";
-import { MouseEventHandler } from "react";
 
 type RequestJsonBoxPropsType = {
     title: Array<string>;
@@ -8,12 +7,12 @@ type RequestJsonBoxPropsType = {
 }
 
 const data_request_json_box: RequestJsonBoxPropsType = {
-    title: [ "Request JSON" ],
-    buttonReset: [ <Link href={"javascript:;"} key="btn-reset">
+    title: ["Request JSON"],
+    buttonReset: [<Link href={"javascript:;"} key="btn-reset">
         <a id="playground-reset-btn">
             <Button text={"Reset Connection"} />
         </a>
-    </Link> ],
+    </Link>],
 };
 
 export default data_request_json_box;
